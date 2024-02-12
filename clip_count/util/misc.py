c651f068153f60aa19715603fc899b1c29cadb05
Oz Zafar <ozzafar@microsoft.com>
2024-02-12 19:33:45 +0200

    fix misc

--- clip_count/util/misc.py
+++ clip_count/util/misc.py
@@ -20,7 +20,6 @@ import argparse
 
 import torch
 import torch.distributed as dist
-from torch._six import inf
 import numpy as np
 import random
 
@@ -288,10 +287,7 @@ def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
     if len(parameters) == 0:
         return torch.tensor(0.)
     device = parameters[0].grad.device
-    if norm_type == inf:
-        total_norm = max(p.grad.detach().abs().max().to(device) for p in parameters)
-    else:
-        total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]), norm_type)
+    total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]), norm_type)
     return total_norm
 
 
